63f5ce430c2a9a292b3aa487fad4508a91934aea
David Sansom Oaxaca <david.oaxaca@hotmail.com>
2016-03-07 09:00:51 -0800

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,7 +45,7 @@ var education = {
 			"major": "Computer Science",
 			"minor": "Germanic Studies",
 			"url": "www.usd.com"
-		}		
+		}
 	],
 	"onlineCourses": [
 		{ "school": "Udacity",
@@ -67,7 +67,7 @@ var education = {
 			"title": "Intro to HTML and CSS",
 			"completed": "October 2015",
 			"url": "https://www.udacity.com/course/ud304"
-		},	
+		},
 		{ "school": "Udacity",
 			"title": "Web Development",
 			"completed": "",
@@ -111,8 +111,8 @@ var formattedWelcomeMsg = HTMLWelcomeMsg.replace("Work, make love, beautiful", b
 
 var formattedContactInfo = [];
 formattedContactInfo.push(HTMLemail.replace("oaxaca.david@hotmail.com%", bio.contacts.email));
-formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
-formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLgithub.replace("https://github.com/DavidOaxaca", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("JoshuaOaxaca", bio.contacts.twitter));
 formattedContactInfo.push(HTMLlocation.replace("palo alto,ca", bio.contacts.location));
 
 
@@ -125,7 +125,7 @@ if(bio.skills.length > 0) {
 	$("#header").append(HTMLskillsStart);
 
 	for(i in bio.skills) {
-		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+		$("#skills").append(HTMLskills.replace("ninja", bio.skills[i]));
 	}
 }
 
@@ -142,11 +142,11 @@ function displayWork() {
 		$("#workExperience").append(HTMLworkStart);
 
 		for(i in work.jobs) {
-			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
-			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
-			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
-			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+			var formattedEmployer = HTMLworkEmployer.replace("The Christmas Light Pros", work.jobs[i].employer);
+			var formattedWorkTitle = HTMLworkTitle.replace("Hanger", work.jobs[i].title);
+			var formattedWorkLocation = HTMLworkLocation.replace("Palo Alto,CA", work.jobs[i].location);
+			var formattedDatesWorked = HTMLworkDates.replace("11/2012-3/15/2016", work.jobs[i].datesWorked);
+			var formattedWorkDescription = HTMLworkDescription.replace("Holiday Lighting company", work.jobs[i].description);
 
 			var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
 
@@ -168,8 +168,8 @@ projects.display = function() {
 		for(i in projects.projects) {
 			$("#projects").append(HTMLprojectStart);
 
-			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
-			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+			var formattedProjectTitle = HTMLprojectTitle.replace("%P1%", projects.projects[i].title).replace("#", projects.projects[i].url);
+			var formattedProjectDates = HTMLprojectDates.replace("%Portfolio%", projects.projects[i].datesWorked);
 			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
 
 			$(".project-entry:last").append(formattedProjectTitle);
@@ -193,12 +193,12 @@ education.display = function() {
 		for(i in education.schools) {
 			$("#education").append(HTMLschoolStart);
 
-			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
-			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
-			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
-			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);			
-			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
-			var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor);
+			var formattedSchoolName = HTMLschoolName.replace("University of San Diego", education.schools[i].name).replace("https://www.usd.com/", education.schools[i].url);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("International Business Management", education.schools[i].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("08/2010-12/12", education.schools[i].datesAttended);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("San Diego,CA", education.schools[i].location);
+			var formattedSchoolMajor = HTMLschoolMajor.replace("Business Managenent", education.schools[i].major);
+			var formattedSchoolMinor = HTMLschoolMinor.replace("Mass Communications", education.schools[i].minor);
 
 			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
 			$(".education-entry:last").append(formattedSchoolDates);
@@ -209,12 +209,12 @@ education.display = function() {
 
 		if(education.onlineCourses.length > 0) {
 			$("#education").append(HTMLonlineClasses);
-			for(i in education.onlineCourses) {				
+			for(i in education.onlineCourses) {
 				$("#education").append(HTMLschoolStart);
-				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
-				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
-				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
-				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("Front-end developer", education.onlineCourses[i].title).replace("https://www.udacity.com/", education.onlineCourses[i].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("Udacity", education.onlineCourses[i].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("11/15-current", education.onlineCourses[i].completed);
+				var formattedOnlineURL = HTMLonlineURL.replace("https://www.udacity.com/", education.onlineCourses[i].url).replace("https://www.udacity.com/", education.onlineCourses[i].url);
 
 				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
 				$(".education-entry:last").append(formattedOnlineDates);
@@ -228,127 +228,143 @@ education.display = function() {
 education.display();
 
 /**
- * Skills Chart
- */
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'scatter',
-            zoomType: 'xy'
-        },
-        title: {
-            text: 'Skills'
-        },
-        xAxis: {
-            title: {
-                enabled: true,
-                text: 'Used (year)'
-            },
-            startOnTick: true,
-            endOnTick: true,
-            showLastLabel: true
-        },
-        yAxis: {
-            title: {
-                text: 'Duration (months)'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 250,
-            y: 0,
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-            borderWidth: 1
-        },
-        plotOptions: {
+  * Skills Chart
+  */
+ $(function () {
+     $('#container').highcharts({
+       chart: {
+             type: 'scatter',
+             zoomType: 'xy'
+    },
+         title: {
+             text: 'Skills'd
+         },
+xAxis: {
+             title: {
+                 enabled: true,
+ text: 'Used (year)'
+             },
+             startOnTick: true,
+   endOnTick: true,
+             showLastLabel: true
+         },
+yAxis: {
+             title: {
+                 text: 'Duration (months)'
+        }
+         },
+         legend: {
+             layout: 'vertical',
+        align: 'left',
+             verticalAlign: 'top',
+             x:
+250,
+             y: 0,
+             floating: true,
+backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor)
+|| '#FFFFFF',
+             borderWidth: 1
+         },
+         plotOptions: {
             scatter: {
-                marker: {
-                    radius: 5,
-                    states: {
-                        hover: {
-                            enabled: true,
-                            lineColor: 'rgb(100,100,100)'
-                        }
-                    }
-                },
-                states: {
-                    hover: {
-                        marker: {
-                            enabled: false
-                        }
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<b>{series.name}</b><br>',
-                    pointFormat: 'Used in {point.x} for {point.y} month(s)'
-                }
-			}
-		},
-		series: [{
-				name: 'Java',
-				color: 'rgba(223, 83, 83, .5)',
-				data: [[2014, 11], [2013, 11], [2012, 11], [2011, 8], [2010, 4], [2009, 12], [1998, 8], [1999, 8], [2000, 4]]
-			},
-			{
+                 marker: {
+                     radius:
+5,
+                     states: {
+                         hover: {
+                  enabled: true,
+                             lineColor:
+'rgb(100,100,100)'
+                         }
+                     }
+       },
+                 states: {
+                     hover: {
+             marker: {
+                             enabled: false
+             }
+                     }
+                 },
+tooltip: {
+                     headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: 'Used in {point.x} for {point.y} month(s)'
+        }
+ 			}
+ 		},
+ 		series: [{
+ 				name: 'Java',
+ 				color: 'rgba(223,
+83, 83, .5)',
+ 				data: [[2014, 11], [2013, 11], [2012, 11], [2011, 8],
+[2010, 4], [2009, 12], [1998, 8], [1999, 8], [2000, 4]]
+ 			},
+ 			{
 				name: 'python',
-				color: 'rgba(119, 152, 191, .5)',
-				data: [[2014, 1], [2013, 1], [2012, 1], [2011, 1]]
-			},
-			{
-				name: 'SQL',
+ 				color: 'rgba(119, 152, 191, .5)',
+ 				data: [[2014,
+1], [2013, 1], [2012, 1], [2011, 1]]
+ 			},
+ 			{
+ 				name: 'SQL',
 				color: 'rgba(119, 152, 80, .5)',
-				data: [[2013, 2]]
-			},
-			{
+ 				data: [[2013, 2]]
+ 			},
+ 			{
 				name: 'PL/SQL',
-				color: 'rgba(170, 162, 30, .5)',
-				data: [[2010, 2], [2006, 4]]
-			},
-			{
-				name: 'C++',
+ 				color: 'rgba(170, 162, 30, .5)',
+ 				data: [[2010,
+2], [2006, 4]]
+ 			},
+ 			{
+ 				name: 'C++',
+ 				color: 'rgba(0, 102, 0,
+.5)',
+ 				data: [[2004, 12], [2003, 6]]
+ 			},
+ 			{
+ 				name: 'C',
 				color: 'rgba(0, 102, 0, .5)',
-				data: [[2004, 12], [2003, 6]]
-			},
-			{
-				name: 'C',
-				color: 'rgba(0, 102, 0, .5)',
-				data: [[1997, 8], [2004, 2], [2008, 4]]
-			},
-			{
-				name: 'Javascript',
-				color: 'rgba(204, 0, 0, .5)',
+ 				data: [[1997, 8], [2004, 2], [2008,
+4]]
+ 			},
+ 			{
+ 				name: 'Javascript',
+ 				color: 'rgba(204, 0, 0, .5)',
 				data: [[2014, 1]]
-			},
-			{
-				name: 'HTML/CSS',
-				color: 'rgba(153, 51, 0, .5)',
-				data: [[2014, 5], [2002, 3], [2008, 4], [2005, 2]]
-			},
-			{
-				name: 'Web Development',
-				color: 'rgba(0, 153, 102, .5)',
-				data: [[2014, 2]]
-			},
-			{
-				name: 'TDD',
-				color: 'rgba(0, 102, 51, .5)',
-				data: [[2014, 12], [2013, 12], [2012, 12], [2011, 4]]
-			},
-			{
-				name: 'Django',
-				color: 'rgba(204, 102, 0, .5)',
+ 			},
+ 			{
+ 				name: 'HTML/CSS',
+ 				color:
+'rgba(153, 51, 0, .5)',
+ 				data: [[2014, 5], [2002, 3], [2008, 4], [2005,
+2]]
+ 			},
+ 			{
+ 				name: 'Web Development',
+ 				color: 'rgba(0, 153, 102,
+.5)',
+ 				data: [[2014, 2]]
+ 			},
+ 			{
+ 				name: 'TDD',
+ 				color:
+'rgba(0, 102, 51, .5)',
+ 				data: [[2014, 12], [2013, 12], [2012, 12], [2011,
+4]]
+ 			},
+ 			{
+ 				name: 'Django',
+ 				color: 'rgba(204, 102, 0, .5)',
 				data: [[2013, 2.5]]
-			},
-			{
-				name: 'Perl',
-				color: 'rgba(210, 200, 0, .5)',
-				data: [[2011, 3], [2013, 1], [2012, 1], [2014, 1]]
-			}]
+ 			},
+ 			{
+ 				name: 'Perl',
+ 				color: 'rgba(210,
+200, 0, .5)',
+ 				data: [[2011, 3], [2013, 1], [2012, 1], [2014, 1]]
+ 			}]
 	});
-});
+ });
 
 
 $("#mapDiv").append(googleMap);
